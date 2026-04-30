a5282f4e0846d26ead022110f2c1dfeb7d7b5537
tryproxy <tryproxy@protonmail.com>
2026-04-30 04:16:34 +0300

    refactor: fix lint violations after ESLint setup

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -11,13 +11,16 @@ describe('App', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it('should render title', async () => {
     const fixture = TestBed.createComponent(App);
+
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
+
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, film-collection');
   });
 });
